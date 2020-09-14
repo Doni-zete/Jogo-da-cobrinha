fa073b00a04313caac52d4767220282ec5562e60
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-14 19:32:03 -0300

    criação do scrip

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+
+
+    let stage = document.getElementById('stage')
+    let cxt = stage.getContext("2d")
+
+    setInterval(game, 1000 / 15)
+
+    const vel = 1
+
+    let velocidadex, velocidadey = 0
+    let posicaox, posicaoy = 10
+    let larguraQuadrado = 20
+    let quantidadePeca = 20
+    let posicaoMacaX, posicaoMacaY = 15
+
+    let trail=[]
+    tail = 5
+
+    function game() {
+
+        posicaox += velocidadex
+        posicaoy +=velocidadey
+        if(posicaox <0){
+            posicaox =quantidadePeca-1
+
+        }
+        if (posicaox > quantidadePeca-1)
+        po
+
+        cxt.fillStyle = "black"
+        cxt.fillRect(0, 0, stage.clientWidth, stage.height)
+    }
+
+
+}
