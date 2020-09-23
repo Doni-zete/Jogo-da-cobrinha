0e202cf062452d5096e3a859fd0068aefb51cdc4
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-22 22:34:41 -0300

    Andamento

--- script.js
+++ script.js
@@ -3,20 +3,42 @@ window.onload = function () {
     let stage = document.getElementById('stage');
     let ctx = stage.getContext("2d");
 
-    setInterval(game, 1000/15);
+    setInterval(game, 60);
 
 
     const vel = 1;
 
-    let velocidadeX = 0;
-    let velocidadeY = 0;
+    let vX = 0;
+    let vY = 0;
+    let px = 10;
+    let py = 15;
+    let lp = 20;
+    let qp = 20;
+    let ax= 15;
+    let ay= 15;
 
-
+    let trail = [];
+    tail = 5;
 
     function game(){
+        px += vX;
+        py += vY;
+        if(px <0){
+            px = qp-1; 
+        }
+        if (px> qp-1){
+            px = 0;
+        }
+        if(py < 0){
+            py = qp-1;
+        }
+        if (py> qp-1){
+            py = 0;
+        }
+    
+    
 
-
-    }
+   
 
 
 
@@ -27,5 +49,5 @@ window.onload = function () {
     
     ctx.fillStyle = "purple";
     ctx.fillRect(0,0, stage.width, stage.height);
-
+}
 }
